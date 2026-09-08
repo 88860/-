744c65786b48200b555e7e99cdabb94fd5853330
𝘼𝙎𝙃𝙀 <111343457+88860@users.noreply.github.com>
2026-09-08 19:06:36 +0800

    Update lxc-s.sh

--- lxc-s.sh
+++ lxc-s.sh
@@ -1505,7 +1505,7 @@ wg_setup(){
       listen_port=$(prompt_port "u" "") || return
       break
     done
-    peer_key=$(prompt "客户端公钥")
+    peer_key=$(prompt "客户端公钥 (留空稍后回填)")
     
     body=$(jq -n --arg private "$private" --arg public "$public" --arg a4 "$address4" --arg a6 "$address6" \
           --argjson port "$listen_port" --arg peer_key "$peer_key" \
