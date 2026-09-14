af0a7119bf487e32e74bfb013a47962f48a84c9c
𝘼𝙎𝙃𝙀 <111343457+88860@users.noreply.github.com>
2026-09-15 03:28:05 +0800

    Fix typo in OpenRC compatibility message

--- lxc-s.sh
+++ lxc-s.sh
@@ -2163,7 +2163,7 @@ while :; do
   clear
   tell "${CYAN}================================${PLAIN}"
   tell "${CYAN}             s管理              ${PLAIN}"
-  tell "${CYAN}      [ 仅适配 OpenRC 版 ]      ${PLAIN}"
+  tell "${CYAN}       [ 仅适配 OpenRC ]        ${PLAIN}"
   tell "${CYAN}================================${PLAIN}"
   tell "  1. 服务端管理"
   tell "  2. 客户端管理"
