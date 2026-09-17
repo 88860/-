38d28c27fa5a1d251bd5969aace02f7a45ac284f
𝘼𝙎𝙃𝙀 <111343457+88860@users.noreply.github.com>
2026-09-17 20:51:08 +0800

    Refactor DNS block configuration in s.sh

--- s.sh
+++ s.sh
@@ -664,7 +664,7 @@ build_config(){
 
   if [ "$use_tun" = 1 ]; then auto_detect="true"; else auto_detect="false"; fi
 
-    dns_block=$(jq -n --arg host "$peer_host" \
+      dns_block=$(jq -n --arg host "$peer_host" \
                     --arg cf4 "$dns_cf_v4" --arg cf6 "$dns_cf_v6" \
                     --arg gg4 "$dns_google_v4" --arg gg6 "$dns_google_v6" \
                     --arg dns_detour "$dns_detour" \
@@ -688,15 +688,15 @@ build_config(){
     else [] end) as $node_rules |
   (
   [
-    {type:"udp",tag:"dns-cf-v4",server:$cf4,server_port:53,detour:$dns_detour,strategy:$dns_strategy},
-    {type:"udp",tag:"dns-google-v4",server:$gg4,server_port:53,detour:$dns_detour,strategy:$dns_strategy},
-    {type:"udp",tag:"dns-direct-cf-v4",server:$cf4,server_port:53,detour:"direct",strategy:$dns_strategy},
-    {type:"udp",tag:"dns-direct-google-v4",server:$gg4,server_port:53,detour:"direct",strategy:$dns_strategy}
+    {type:"udp",tag:"dns-cf-v4",server:$cf4,server_port:53,detour:$dns_detour},
+    {type:"udp",tag:"dns-google-v4",server:$gg4,server_port:53,detour:$dns_detour},
+    {type:"udp",tag:"dns-direct-cf-v4",server:$cf4,server_port:53,detour:"direct"},
+    {type:"udp",tag:"dns-direct-google-v4",server:$gg4,server_port:53,detour:"direct"}
   ] + (if $has_v6 == 1 then [
-    {type:"udp",tag:"dns-cf-v6",server:$cf6,server_port:53,detour:$dns_detour,strategy:$dns_strategy},
-    {type:"udp",tag:"dns-google-v6",server:$gg6,server_port:53,detour:$dns_detour,strategy:$dns_strategy},
-    {type:"udp",tag:"dns-direct-cf-v6",server:$cf6,server_port:53,detour:"direct",strategy:$dns_strategy},
-    {type:"udp",tag:"dns-direct-google-v6",server:$gg6,server_port:53,detour:"direct",strategy:$dns_strategy}
+    {type:"udp",tag:"dns-cf-v6",server:$cf6,server_port:53,detour:$dns_detour},
+    {type:"udp",tag:"dns-google-v6",server:$gg6,server_port:53,detour:$dns_detour},
+    {type:"udp",tag:"dns-direct-cf-v6",server:$cf6,server_port:53,detour:"direct"},
+    {type:"udp",tag:"dns-direct-google-v6",server:$gg6,server_port:53,detour:"direct"}
   ] else [] end)
 ) as $servers |
     {
@@ -716,10 +716,9 @@ build_config(){
       + (if $has_v6 == 1 then [
         {match_response:"remote-aaaa",action:"route",server:"dns-cf-v6",race:true},
         {match_response:"remote-gg-aaaa",action:"route",server:"dns-google-v6",race:true}
-      ] else [] end))
-    }'
-)
-
+      ] else [] end)),
+      "strategy": $dns_strategy
+    }')
 
   jq -n --argjson inbounds "$inbounds" --argjson outbounds "$outbounds" \
         --argjson endpoints "$endpoints" --argjson rules "$rules" \
